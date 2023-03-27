afc70d4ebc7bcba96ef72f5180f125c57535318a
Naz Azhar <57789624+nazhar95@users.noreply.github.com>
2023-03-27 09:01:25 +0800

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -100,7 +100,7 @@
 			href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
 			rel="stylesheet"
 		/>
-		<link rel="stylesheet" href="./style.css" />
+		<link rel="stylesheet" href="style.css" />
 		<title>Frozen Delights Ice-Cream Shop</title>
 	</head>
 
